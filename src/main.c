73abe3895d316b45531e458d87348db0552d47a9
DavierSB <daviersanbell@gmail.com>
2023-04-27 21:29:46 -0400

    history

--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@ void Loop();
 void Iniciar_History(GQueue *);
 int Size(char **);
 void Guardar_History(GQueue *);
+void Quitar_Salto_de_Linea(char *);
 
 int main()
 {
@@ -54,7 +55,10 @@ void Iniciar_History(GQueue *history)
     size_t len = 0;
     FILE *file = fopen("./history.txt", "r");
     while ((getline(&line, &len, file)) != -1)
+    {
+        Quitar_Salto_de_Linea(line);
         g_queue_push_head(history, g_strdup(line));
+    }
     fclose(file);
 }
 void Guardar_History(GQueue *history)
@@ -73,4 +77,13 @@ int Size(char** arr)
     int count = 0;
     for(; arr[count] != NULL; count++);
     return count;
+}
+void Quitar_Salto_de_Linea(char *line)
+{
+    for (int i = 0; line[i] != NULL; i++)
+        if(line[i] == '\n')
+        {
+            line[i] = NULL;
+            return;
+        }
 }
